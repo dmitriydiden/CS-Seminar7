32c82c284a2e86034372adbe00db5d4a8435d46a
dmitry_di <dnd_d@mail.ru>
2022-05-17 20:33:12 +0300

    Добавили второй вариант решения с помощью методов

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,7 +1,9 @@
 ﻿/* Задайте двумерный массив. 
 Найдите элементы, у которых оба индекса чётные, 
 и замените эти элементы на их квадраты.*/
-using System;
+
+// Моё решение
+/*using System;
 Console.Clear();
 
 int n = int.Parse(Console.ReadLine());
@@ -32,4 +34,55 @@ for (int i = 0; i < matrix.GetLength(0); i++)
     }
 
     Console.WriteLine();
+}
+*/
+
+// Решение Тимура
+using System;
+Console.Clear();
+int[,] startArray = GetRandomArray(4,4,1,10);
+PrintArray(startArray);
+Console.WriteLine();
+startArray = ChangeArray(startArray);
+PrintArray(startArray);
+
+int[,] GetRandomArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i>0 && i%2==0 && j>0 && j%2 ==0)
+            {
+                array[i,j]*=array[i,j];
+            }
+        }
+        
+    }
+    return array;
 }

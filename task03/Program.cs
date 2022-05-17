053858812b487082ba3164b7eecf11fcc7bf1769
dmitry_di <dnd_d@mail.ru>
2022-05-17 19:58:03 +0300

    Написали программу, которая меняет элементы стоящие на четных позициях в двумерном массиве на их квадраты

---
+++ task03/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задайте двумерный массив. 
+Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты.*/
+using System;
+Console.Clear();
+
+int n = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[n,m];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j]=new Random().Next(0,10);
+        Console.Write($"{matrix[i,j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if ((i % 2 == 0) && (j % 2 == 0))
+        {
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+            Console.Write($"{matrix[i, j]} ");
+        }
+        else Console.Write($"{matrix[i, j]} ");
+    }
+
+    Console.WriteLine();
+}

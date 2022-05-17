816cf11a4f6683408cc236b01cd2a30ba3c30bcd
dmitry_di <dnd_d@mail.ru>
2022-05-17 19:34:26 +0300

    Написали программу, которая заполняет двумерный массив элементами равными сумме индекса строки и столбца

---
+++ task02/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится по формуле: 
+Aₘₙ = m+n. Выведите полученный массив на экран.*/
+using System;
+Console.Clear();
+
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
+int [,] matrix = new int[m,n];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j]= (i+1)+(j+1);
+        Console.Write($"{matrix[i,j]} ");
+    }
+    Console.WriteLine();
+}

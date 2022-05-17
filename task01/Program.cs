9692fbaed07a057b2727062573cf8ecbb2c3e828
dmitry_di <dnd_d@mail.ru>
2022-05-17 19:18:30 +0300

    Написали программу заполнения двумерного массива целыми числами

---
+++ task01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Заполнить двумерный массив размером n на m 
+using System;
+Console.Clear();
+
+int n = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        matrix[i, j] = new Random().Next(0, 100);
+        Console.Write($"{matrix[i,j]} ");
+    }
+    Console.WriteLine();
+}
+
